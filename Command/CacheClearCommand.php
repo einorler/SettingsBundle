e1e1a1d5387a710f779b315ba0d09e2d393e0ac4
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-10-10 09:56:15 +0300

    removed unnecessary use statements

--- Command/CacheClearCommand.php
+++ Command/CacheClearCommand.php
@@ -11,12 +11,9 @@
 
 namespace ONGR\SettingsBundle\Command;
 
-use ONGR\ElasticsearchBundle\Service\Manager;
-use ONGR\SettingsBundle\Service\SettingsManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
