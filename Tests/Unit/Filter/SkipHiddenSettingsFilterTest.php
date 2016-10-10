884f467e20401f139371b4374e8d549c896187c3
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-10-10 14:06:35 +0300

    added a test for the filter

---
+++ Tests/Unit/Filter/SkipHiddenSettingsFilterTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ONGR\SettingsBundle\Tests\Unit;
+
+use ONGR\ElasticsearchDSL\Search;
+use ONGR\SettingsBundle\Filter\SkipHiddenSettingsFilter;
+
+class SkipHiddenSettingsFilterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests if the filter modifies search correctly
+     */
+    public function testModifySearch()
+    {
+        $search = new Search();
+        $filter = new SkipHiddenSettingsFilter();
+
+        $filter->modifySearch($search);
+
+        $this->assertEquals(
+            [
+                'query' => [
+                    'bool' => [
+                        'must_not' => [
+                            [
+                                'term' => [
+                                    'type' => 'hidden',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            $search->toArray()
+        );
+    }
+}
