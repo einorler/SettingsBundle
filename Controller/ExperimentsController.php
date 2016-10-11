c7119e4e3487b787cda5380def02e0098a39ae39
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-10-11 10:25:10 +0300

    added experiments controller

---
+++ Controller/ExperimentsController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\SettingsBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class SettingsListController. Is used for managing settings in General env.
+ */
+class ExperimentsController extends Controller
+{
+    public function listAction(Request $request)
+    {
+        return new JsonResponse('experiments!');
+    }
+}
