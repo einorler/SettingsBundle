9cc7f4a99d56b06adedd0b8e6a2879d5b3e3c880
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-10-11 11:57:49 +0300

    added a document for experiment

---
+++ Document/Experiment.php
@@ -0,0 +1,218 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\SettingsBundle\Document;
+
+use ONGR\ElasticsearchBundle\Annotation as ES;
+use ONGR\FilterManagerBundle\SerializableInterface;
+
+/**
+ * @ES\Document(type="experiment")
+ */
+class Experiment implements SerializableInterface
+{
+    /**
+     * @var string
+     *
+     * @ES\Id()
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ES\Property(
+     *  type="string",
+     *  options={
+     *    "fields"={
+     *        "raw"={"type"="string", "index"="not_analyzed"},
+     *        "name"={"type"="string"}
+     *    }
+     *  }
+     * )
+     */
+    private $name;
+
+    /**
+     * @var array
+     *
+     * @ES\Property(
+     *  type="string",
+     *  options={
+     *    "fields"={
+     *        "raw"={"type"="string", "index"="not_analyzed"},
+     *        "profile"={"type"="string"}
+     *    }
+     *  }
+     * )
+     */
+    private $profile = [];
+
+    /**
+     * @var array
+     *
+     * @ES\Property(type="string", options={"index"="not_analyzed"})
+     */
+    private $os = [];
+
+    /**
+     * @var array
+     *
+     * @ES\Property(type="string", options={"index"="not_analyzed"})
+     */
+    private $client = [];
+
+    /**
+     * @var array
+     *
+     * @ES\Property(type="string", options={"index"="not_analyzed"})
+     */
+    private $device = [];
+
+    /**
+     * @var string
+     *
+     * @ES\Property(type="date")
+     */
+    private $createdAt;
+
+    /**
+     * Experiment constructor.
+     */
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return array
+     */
+    public function getProfile()
+    {
+        return $this->profile;
+    }
+
+    /**
+     * @param array $profile
+     */
+    public function setProfile($profile)
+    {
+        $this->profile = $profile;
+    }
+
+    /**
+     * @return array
+     */
+    public function getOs()
+    {
+        return $this->os;
+    }
+
+    /**
+     * @param array $os
+     */
+    public function setOs($os)
+    {
+        $this->os = $os;
+    }
+
+    /**
+     * @return array
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param array $client
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDevice()
+    {
+        return $this->device;
+    }
+
+    /**
+     * @param array $device
+     */
+    public function setDevice($device)
+    {
+        $this->device = $device;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param string $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getSerializableData()
+    {
+        return [
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'profile' => $this->getProfile(),
+            'os' => $this->getOs(),
+            'client' => $this->getClient(),
+            'device' => $this->getDevice()
+        ];
+    }
+}
