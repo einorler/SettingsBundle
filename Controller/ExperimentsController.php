7447ba64d41a55d2cbef1aca7d369aff32f809c2
Mantas Marcinkevičius <marc.mantas@gmail.com>
2016-10-13 15:49:49 +0300

    done controller housekeeping

--- Controller/ExperimentsController.php
+++ Controller/ExperimentsController.php
@@ -11,18 +11,11 @@
 
 namespace ONGR\SettingsBundle\Controller;
 
-use DeviceDetector\DeviceDetector;
-use DeviceDetector\Parser\Client\Browser;
-use DeviceDetector\Parser\Client\ClientParserAbstract;
-use DeviceDetector\Parser\Device\DeviceParserAbstract;
-use DeviceDetector\Parser\OperatingSystem;
-use DeviceDetector\Parser\ParserAbstract;
 use ONGR\SettingsBundle\Document\Setting;
 use ONGR\SettingsBundle\Service\SettingsManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class SettingsListController. Is used for managing settings in General env.
@@ -31,28 +24,7 @@ class ExperimentsController extends Controller
 {
     public function listAction(Request $request)
     {
-        $dd = new DeviceDetector($request->headers->get('User-Agent'));
-        $dd->parse();
-
-        if ($dd->isBot()) {
-            return new JsonResponse('Bots be gon!');
-        }
-
-        $json = json_encode(
-            [
-                'clientInfo' => $dd->getClient(),
-                'osInfo' => $dd->getOs(),
-                'device' => $dd->getDevice(),
-                'brand' => $dd->getBrand(),
-                'model' => $dd->getModel(),
-            ],
-            JSON_PRETTY_PRINT
-        );
-
-//        return new JsonResponse($json, 200, [], true);
-        return $this->render('ONGRSettingsBundle:Experiments:list.html.twig', [
-
-        ]);
+        return $this->render('ONGRSettingsBundle:Experiments:list.html.twig');
     }
 
     /**
@@ -87,37 +59,38 @@ class ExperimentsController extends Controller
      */
     public function getTargetsAction()
     {
-        $targets = [];
-        $targets['Devices'] = [
-            'desktop',
-            'smartphone',
-            'tablet',
-            'car browser',
-            'console',
-            'tv',
-        ];
-        $targets['Clients'] = [
-            'Firefox',
-            'Safari',
-            'Chrome',
-            'Opera',
-            'Edge',
-            'IE Mobile',
-            'Internet Explorer',
-            'Mobile Safari',
-            'Android Browser',
-            'Chrome Mobile',
-            'Chrome Mobile iOS',
-            'Opera Mobile',
-            'UC Browser',
-        ];
-        $targets['OS'] = [
-            'Mac',
-            'Windows',
-            'iOS',
-            'Android',
-            'Ubuntu',
-            'Debian',
+        $targets = [
+            'Devices' => [
+                'desktop',
+                'smartphone',
+                'tablet',
+                'car browser',
+                'console',
+                'tv',
+            ],
+            'Clients' => [
+                'Firefox',
+                'Safari',
+                'Chrome',
+                'Opera',
+                'Edge',
+                'IE Mobile',
+                'Internet Explorer',
+                'Mobile Safari',
+                'Android Browser',
+                'Chrome Mobile',
+                'Chrome Mobile iOS',
+                'Opera Mobile',
+                'UC Browser',
+            ],
+            'OS' => [
+                'Mac',
+                'Windows',
+                'iOS',
+                'Android',
+                'Ubuntu',
+                'Debian',
+            ],
         ];
 
         return new JsonResponse($targets);
